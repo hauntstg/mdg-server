8b0acc5a91bff278da68db2d7588caf43b132a3c
Steven Nguyen <nthau.it96@gmail.com>
2025-11-18 08:50:54 +0700

    Clear code

--- routes/admin/specialty.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const specialtyController = require("../../controllers/admin/specialty");
-
-const router = express.Router();
-
-router.post("/specialty", specialtyController.getSpecialties);
-
-module.exports = router;
